e1ed4975e0129d9a7516423c4db37dffa7bdafbc
arpitsahuu <arpitsahu358@gmail.com>
2024-08-27 18:19:41 +0530

    modify hero router linkd

--- src/components/router/Hero.tsx
+++ src/components/router/Hero.tsx
@@ -19,14 +19,14 @@ const Hero = () => {
       <div className="flex  flex-col items-center sm:flex-row w-full px-5 sm:px-[44px] text-[#003e52] justify-center gap-3 sm:gap-8">
         <Link
           href={"/products"}
-          className="flex gap-2 items-center border border-green-400 rounded-full px-4 py-2"
+          className="flex gap-2 items-center border border-green-400 rounded-full py-1 px-3 sm:px-4 sm:py-2  "
         >
           {" "}
           <FaArrowRight /> Products & Ideas
         </Link>
         <Link
           href={"/newsandevents"}
-          className="flex gap-2 items-center border border-green-400 rounded-full px-4 py-2"
+          className="flex gap-2 items-center border border-green-400 rounded-full py-1 px-3 sm:px-4 sm:py-2 text-lg sm:text-xl"
         >
           {" "}
           <FaArrowRight />
