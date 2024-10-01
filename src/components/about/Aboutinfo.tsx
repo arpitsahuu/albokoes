d5b48edc50e0b6eae2b8166895ff02b1e701058a
arpitsahuu <arpitsahu358@gmail.com>
2024-10-01 13:34:35 +0530

    change in about contact

--- src/components/about/Aboutinfo.tsx
+++ src/components/about/Aboutinfo.tsx
@@ -9,7 +9,7 @@ const Aboutinfo = () => {
           <span className="text-[#00ce7d]">Better World</span>{" "}
         </h1>
         <p className="px-6 sm:px-28 text-lg sm:mt-6 mb-8 mx-auto text-[#00536f]">
-          Albokoes EnviroSustain Pvt. Ltd. (AES), founded in 2022 by three visionary individuals, is driven by a passion for environmental stewardship and a commitment to addressing pressing ecological challenges. The founders bring a unique blend of expertise in environmental science, sustainability, and innovative technology, forming the cornerstone of AES&apos;s mission to integrate cutting-edge solutions for a greener, more sustainable future.
+          Albokoes EnviroSustain Pvt. Ltd. (AES), founded in 2022 by visionary individuals, is driven by a passion for environmental stewardship and a commitment to addressing pressing ecological challenges. The founders bring a unique blend of expertise in environmental science, sustainability, and innovative technology, forming the cornerstone of AES&apos;s mission to integrate cutting-edge solutions for a greener, more sustainable future.
         </p>
         <p className="px-6 sm:px-28 text-lg sm:mt-6 mt-3 mb-8 mx-auto text-[#00536f]">
           The collective vision and dedication of the founders aim to position AES as a leader in fostering a harmonious balance between human progress and ecological preservation. By leveraging their wealth of knowledge and commitment to innovative environmental solutions, AES lays the foundation for sustainable development.
