79f53a55a7f6b8d753a29d2326da8b9647d29e5b
arpitsahuu <arpitsahu358@gmail.com>
2024-10-02 17:37:50 +0530

    change the service card

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -14,8 +14,8 @@ type ProductCardProps = {
 const ProductCard: React.FC<ProductCardProps> = ({ image, title, category, discription,link }) => {
     return (
         <article className="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
-            <a
-                href="#"
+            <Link
+                href={link}
                 className="group shrink-0 relative block h-56 w-full self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40"
             >
                 <img
@@ -24,16 +24,16 @@ const ProductCard: React.FC<ProductCardProps> = ({ image, title, category, discr
                     alt=""
                     className="group-hover:scale-110 absolute inset-0 h-full w-full object-cover object-center transition duration-200"
                 />
-            </a>
+            </Link>
             <div className="flex flex-col gap-2">
                 <span className="text-sm text-gray-400">{category}</span>
                 <h2 className="text-xl font-bold text-gray-800">
-                    <a
-                        href="#"
+                    <Link
+                        href={link}
                         className=" transition duration-100 hover:text-green-500"
                     >
                         {title}
-                    </a>
+                    </Link>
                 </h2>
                 <p className="text-gray-500 line-clamp-3">
                     {discription}
